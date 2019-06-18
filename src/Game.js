d3ea28b7df887b33a585871876de8cabf62832e2
Dong Yuwei <newdongyuwei@gmail.com>
2019-06-18 10:15:32 +0800

    use native dom to update the grid. CPU from 120%~130% to 17%

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,11 @@ class Game extends Component {
     super(props);
     this.interval = props.interval;
     this.lifeGame = new LifeGame(props.rows, props.columns);
+    this.tableCells = [];
+    for (var i = 0; i < props.rows; i++) {
+      this.tableCells[i] = [];
+    }
+
     this.state = {
       grid: this.lifeGame.reset(),
       liveCells: 0
@@ -34,10 +39,21 @@ class Game extends Component {
 
   continue = () => {
     clearInterval(this.timer);
+    const game = this.lifeGame;
     this.timer = setInterval(() => {
-      this.setState({
-        grid: this.lifeGame.iterate()
-      });
+      const grid = game.iterate();
+      const rows = game.rows;
+      const columns = game.columns;
+      for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < columns; j++) {
+          const cellEl = this.tableCells[i][j];
+          const cell = grid[i][j];
+          if (cellEl.dataset.state !== grid[i][j].state.toString()) {
+            cellEl.className = cell.state === 1 ? "live" : "dead";
+            cellEl.dataset.state = cell.state;
+          }
+        }
+      }
     }, this.interval);
   };
 
@@ -68,6 +84,9 @@ class Game extends Component {
     });
   }
 
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return false;
+  }
   render() {
     const { grid } = this.state;
     return (
@@ -92,11 +111,16 @@ class Game extends Component {
                   {rows.map((cell, j) => {
                     return (
                       <td
+                        id={`cell-${i}-${j}`}
+                        data-state={cell.state}
                         key={j}
                         onClick={e => {
                           this.toggleCellState(cell);
                         }}
                         className={cell.state === 1 ? "live" : "dead"}
+                        ref={el => {
+                          this.tableCells[i][j] = el;
+                        }}
                       />
                     );
                   })}
